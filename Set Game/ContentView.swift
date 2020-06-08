a39b4429b1c5c6ab2c77639fe5e958133efe81d6
Ivan <ivan.martino@ies2.com.br>
2020-06-08 08:44:21 -0300

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Ivan on 08/06/20.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
